7461ff907e3edeaa76c18d3a05bd5ce784f78e24
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-04 12:18:11 -0300

    Moved repository for typeORM access

--- src/database/migrations/1591229051993-CreateAppointments.ts
+++ src/database/migrations/1591229051993-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1591229051993
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
