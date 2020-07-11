dee37a43b3220d9b362075d1cdd8299ef6bd31be
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-07-11 12:24:59 -0300

    Add passwd recovery by email tests

---
+++ src/modules/users/services/SendForgottenPasswdEmailService.spec.ts
@@ -0,0 +1,64 @@
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+import SendForgottenPasswdEmailService from './SendForgottenPasswdEmailService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let fakeMailProvider: FakeMailProvider;
+let sendForgottenPasswdEmail: SendForgottenPasswdEmailService;
+
+describe('SendForgottenPasswdEmail', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    sendForgottenPasswdEmail = new SendForgottenPasswdEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('should be able to recover password using email', async () => {
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    await sendForgottenPasswdEmail.execute({
+      email: 'johndoe@email.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover a non-existing user password', async () => {
+    await expect(
+      sendForgottenPasswdEmail.execute({
+        email: 'johndoe@email.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a forgotten password token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokensRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    await sendForgottenPasswdEmail.execute({
+      email: 'johndoe@email.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
+  });
+});
---
+++ src/modules/users/services/SendForgottenPasswdEmailService.ts
@@ -0,0 +1,40 @@
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgottenPasswdEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgottenPasswdEmailService;
