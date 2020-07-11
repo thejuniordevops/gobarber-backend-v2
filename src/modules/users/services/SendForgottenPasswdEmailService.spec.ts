3c5db2d48aa9ee322dfe27415bdec5eadb8eeec4
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-07-11 19:01:21 -0300

    Add passwd reset test and fixed email test

--- src/modules/users/services/SendForgottenPasswdEmailService.spec.ts
+++ src/modules/users/services/SendForgottenPasswdEmailService.spec.ts
@@ -17,8 +17,8 @@ describe('SendForgottenPasswdEmail', () => {
 
     sendForgottenPasswdEmail = new SendForgottenPasswdEmailService(
       fakeUsersRepository,
-      fakeMailProvider,
       fakeUserTokensRepository,
+      fakeMailProvider,
     );
   });
 
