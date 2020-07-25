2bc35d94625520487524e9989c3d859a44c83e0b
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-07-25 11:37:01 -0300

    Add list provider available days

--- src/modules/appointments/services/ListProviderMonthAvailability.ts
+++ src/modules/appointments/services/ListProviderMonthAvailability.ts
@@ -1,3 +1,4 @@
+import { getDate, getDaysInMonth } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -24,7 +25,7 @@ class ListProviderMonthAvailabilityService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    const appointments = this.appointmentsRepository.findAllInMonthFromProvider(
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
         year,
@@ -32,7 +33,25 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    return appointments;
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
